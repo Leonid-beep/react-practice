b9c0215e8912dae5f8c8d6e64928d662e89f7a5a
Usachev Leonid <leonidusachev04@yandex.ru>
2025-04-23 13:23:53 +0300

    project is done

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,4 +1,5 @@
-import { Text } from 'components/text';
+import clsx from 'clsx';
+import { Text } from '../../components/text';
 
 import styles from './Button.module.scss';
 
@@ -12,7 +13,14 @@ export const Button = ({
 	type?: React.ButtonHTMLAttributes<HTMLButtonElement>['type'];
 }) => {
 	return (
-		<button className={styles.button} type={type} onClick={onClick}>
+		<button 
+			className={clsx(
+				styles.button,
+				type === 'submit' && styles.submit,
+				type === 'reset' && styles.reset
+			)} 
+			type={type}
+			onClick={onClick}>
 			<Text weight={800} uppercase>
 				{title}
 			</Text>
